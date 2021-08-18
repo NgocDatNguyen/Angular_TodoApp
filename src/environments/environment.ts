060499f3a1bf34f59c41315d772b4e01d0e151b7
Ngoc Dat Nguyen <ngocdat99hl@gmail.com>
2021-08-18 15:45:21 +0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyADLNdsdjnPSnwpUoCqD3TPvMdZyL32H1c",
+    authDomain: "todo-4def9.firebaseapp.com",
+    databaseURL: "https://todo-4def9-default-rtdb.firebaseio.com",
+    projectId: "todo-4def9",
+    storageBucket: "todo-4def9.appspot.com",
+    messagingSenderId: "32271960173",
+    appId: "1:32271960173:web:a8fafba6d321ae48e45764",
+    measurementId: "G-JC8BS7T74Q"
+  }
 };
 
 /*
